8d6de5f79acffa505ebec664c2f0ea2a00ebdeec
qwerty111i <adwaithsramesh@gmail.com>
2025-08-22 22:52:43 -0400

    Add missing import

--- frontend/src/pages/Panel/Panel.jsx
+++ frontend/src/pages/Panel/Panel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase';
